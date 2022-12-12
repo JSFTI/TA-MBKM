8f6eb528cd23828e1e61541b3c0d71df8a6e7349
JSFTI <jasonsuryaf@gmail.com>
2022-12-12 17:02:08 +0700

    Change URL

--- server/public/panel/assets/new-7ba1da45.js
+++
@@ -1,2 +0,0 @@
-import{_ as c}from"./ProductDetailForm.vue_vue_type_script_setup_true_lang-82e4314e.js";import{_ as n,aj as _,X as s,o as m,t as d,w as t,a as o}from"./vendor-d8963f8a.js";import"./ACurrencyInput.vue_vue_type_script_setup_true_lang-cebbc278.js";import"./lodash-89c1e3b6.js";import"./main-38cd24a7.js";const i={};function p(f,l){const e=c,a=_,r=s;return m(),d(r,{title:"New Product"},{default:t(()=>[o(a,null,{default:t(()=>[o(e)]),_:1})]),_:1})}const h=n(i,[["render",p]]);export{h as default};
-//# sourceMappingURL=new-7ba1da45.js.map
