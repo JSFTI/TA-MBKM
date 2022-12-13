b93a6fe80105d068049b844888a6ab7f5351f940
JSFTI <jasonsuryaf@gmail.com>
2022-12-14 01:49:51 +0700

    HTTPS build

---
+++ server/public/panel/assets/new-8b3f1ab6.js
@@ -0,0 +1,2 @@
+import{_ as c}from"./ProductDetailForm.vue_vue_type_script_setup_true_lang-ef20bde5.js";import{_ as n,aj as _,X as s,o as m,t as d,w as t,a as o}from"./vendor-d8963f8a.js";import"./ACurrencyInput.vue_vue_type_script_setup_true_lang-cebbc278.js";import"./lodash-89c1e3b6.js";import"./main-6b47012f.js";const i={};function p(f,l){const e=c,a=_,r=s;return m(),d(r,{title:"New Product"},{default:t(()=>[o(a,null,{default:t(()=>[o(e)]),_:1})]),_:1})}const h=n(i,[["render",p]]);export{h as default};
+//# sourceMappingURL=new-8b3f1ab6.js.map
